8c168367edea364ca9c6c5ffed2145b6cddb0a5d
ThorstiFinki <105248638+ThorstiFinki@users.noreply.github.com>
2023-07-31 19:42:35 +0200

    App weiter bearbeitet.

---
+++ app/src/main/java/com/example/weatherforecast/screens/main/MainScreen.kt
@@ -0,0 +1,10 @@
+package com.example.weatherforecast.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun MainScreen(navController: NavController){
+    Text(text = "Main Screen")
+}
---
+++ app/src/main/java/com/example/weatherforecast/utils/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.weatherforecast.utils
+
+object Constants {
+}
